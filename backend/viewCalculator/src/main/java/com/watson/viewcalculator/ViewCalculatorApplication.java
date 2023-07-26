e40893a727818774ed7bd4baff835581d975928c
zeomzzz <jmlee2705@gmail.com>
2023-07-26 17:41:43 +0900

    init: notice, viewCalculator 프로젝트 생성

---
+++ backend/viewCalculator/src/main/java/com/watson/viewcalculator/ViewCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.watson.viewcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViewCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViewCalculatorApplication.class, args);
+	}
+
+}
