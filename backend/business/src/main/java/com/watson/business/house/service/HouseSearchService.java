c8c656fada918dab61897264bf9b30f1416e2561
박지영 <pjy7814@naver.com>
2023-07-30 13:09:32 +0900

    feat: 매물Id로 매물 정보 받아오기 Service 개발

--- backend/business/src/main/java/com/watson/business/house/service/HouseSearchService.java
+++ backend/business/src/main/java/com/watson/business/house/service/HouseSearchService.java
@@ -1,21 +1,26 @@
 package com.watson.business.house.service;
 
+import com.watson.business.exception.NotFoundException;
 import com.watson.business.house.domain.entity.House;
-import com.watson.business.house.domain.entity.houseinfo.MonthlyInfos;
-import com.watson.business.house.domain.entity.houseinfo.SaleInfos;
-import com.watson.business.house.domain.entity.houseinfo.YearlyInfos;
 import com.watson.business.house.domain.repository.HouseRepository;
 import com.watson.business.house.dto.search.HouseListResponse;
+import com.watson.business.house.dto.search.HouseResponse;
+import com.watson.business.house.dto.search.RealtorResponse;
+import com.watson.business.region.service.RegionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class HouseSearchService {
     @Autowired
     private HouseRepository houseRepository;
+
+    @Autowired
+    private RegionService regionService;
 
     public List<HouseListResponse> searchAllHouse() {
         List<House> houseList = houseRepository.findAll();
@@ -34,20 +39,63 @@ public class HouseSearchService {
                     houseListResponse.setDeposit(h.getYearlyInfos().getDeposit());
                     houseListResponse.setMaintenance(h.getYearlyInfos().getMaintenance());
                 }
-                case 3 -> {     // 매매
-                    houseListResponse.setSalePrice(h.getSaleInfos().getSalePrice());
-                }
-                default -> {
-                    // 오류
-                }
+                case 3 -> houseListResponse.setSalePrice(h.getSaleInfos().getSalePrice());      // 매매
+                default -> throw new NotFoundException(404);
             }
-            // 사진 정보 관련 기능 구현
+            // TODO: 사진 정보 관련 기능 구현
 
-            // access-key 존재시 isWish 관련 기능 구현  : 매물(h.getId()) + wish테이블
+            // TODO: access-key 존재시 isWish 관련 기능 구현  : 매물(h.getId()) + wish테이블
 
             allHouseList.add(houseListResponse);
         }
 
         return allHouseList;
+    }
+
+    public HouseResponse searchHouseById(Long houseId) {
+        Optional<House> optionalHouse = houseRepository.findById(houseId);
+        if (optionalHouse.isEmpty()) {
+            throw new NotFoundException(404);
+        }
+        House house = optionalHouse.get();
+
+        RealtorResponse realtorResponse = new RealtorResponse(null, house.getRealtorId());  // realtorName 로직 필요
+        HouseResponse houseResponse = new HouseResponse(
+                realtorResponse,
+                house.getId(),
+                house.getContractCode(),
+                regionService.getDongleeNameByEmdCode(house.getDongCode()),
+                house.getHouseCode(),
+                house.getSupplyAreaMeter(),
+                house.getSquareMeter(),
+                house.getFloor(),
+                house.getTotalFloor(),
+                house.getAddress(),
+                house.getBuildingUse(),
+                house.getApprovalBuildingDate(),
+                house.getBathroom(),
+                house.getTitle(),
+                house.getContent(),
+                house.getStatus(),
+                house.getHouseFiles(),
+                house.getRegDate(),
+                house.getHouseOption()
+        );
+        switch (house.getContractCode()) {
+            case 1 -> {     // 월세
+                houseResponse.setDeposit(house.getMonthlyInfos().getDeposit());
+                houseResponse.setMaintenance(house.getMonthlyInfos().getMaintenance());
+            }
+            case 2 -> {     // 전세
+                houseResponse.setDeposit(house.getYearlyInfos().getDeposit());
+                houseResponse.setMaintenance(house.getYearlyInfos().getMaintenance());
+            }
+            case 3 -> houseResponse.setSalePrice(house.getSaleInfos().getSalePrice());       // 매매
+            default -> throw new NotFoundException(404);
+        }
+
+        // TODO: isWish 로직 필요
+
+        return houseResponse;
     }
 }
