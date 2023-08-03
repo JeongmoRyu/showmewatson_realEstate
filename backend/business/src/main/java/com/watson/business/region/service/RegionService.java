9e93cb8e82f7a962fe5e2ae40967f0aea3df10c8
OhChaeYoung <oh5chaeyoung@gmail.com>
2023-08-03 09:42:36 +0900

    fix: Restore region feature

--- backend/business/src/main/java/com/watson/business/region/service/RegionService.java
+++ backend/business/src/main/java/com/watson/business/region/service/RegionService.java
@@ -10,30 +10,30 @@ import java.util.List;
 
 @Service
 public class RegionService {
-//    @Autowired
-//    private EmdRepository emdRepository;
-//
-//    // 군구 불러오기
-//    public List<String> getGunguNamesBySido(String sido) {
-//        return emdRepository.findGunguNamesBySido(sido);
-//    }
-//
-//    // 법정동코드 불러오기
-//    public List<EmdResponse> getCourtCodesByGungu(String gunguName) {
-//        List<Object[]> courtCodesByGungu = emdRepository.findCourtCodesByGungu(gunguName);
-//        List<EmdResponse> emdResponseList = new ArrayList<>();
-//
-//        for(Object[] court : courtCodesByGungu) {
-//            String courtCode = (String) court[0];
-//            String dongleeName = (String) court[1];
-//            EmdResponse emdResponse = new EmdResponse(courtCode, dongleeName);
-//            emdResponseList.add(emdResponse);
-//        }
-//
-//        return emdResponseList;
-//    }
-//
-//    public EmdResponse getDongleeNameByEmdCode(String courtCode) {
-//        return new EmdResponse(emdRepository.findDongleeNameByCourtCode(courtCode), courtCode);
-//    }
+    @Autowired
+    private EmdRepository emdRepository;
+
+    // 군구 불러오기
+    public List<String> getGunguNamesBySido(String sido) {
+        return emdRepository.findGunguNamesBySido(sido);
+    }
+
+    // 법정동코드 불러오기
+    public List<EmdResponse> getCourtCodesByGungu(String gunguName) {
+        List<Object[]> courtCodesByGungu = emdRepository.findCourtCodesByGungu(gunguName);
+        List<EmdResponse> emdResponseList = new ArrayList<>();
+
+        for(Object[] court : courtCodesByGungu) {
+            String courtCode = (String) court[0];
+            String dongleeName = (String) court[1];
+            EmdResponse emdResponse = new EmdResponse(courtCode, dongleeName);
+            emdResponseList.add(emdResponse);
+        }
+
+        return emdResponseList;
+    }
+
+    public EmdResponse getDongleeNameByEmdCode(String courtCode) {
+        return new EmdResponse(emdRepository.findDongleeNameByCourtCode(courtCode), courtCode);
+    }
 }
