3464c44ade410a8c0ffbe33877e966abbf412399
오채영 <web3765@gmail.com>
2023-08-03 17:45:53 +0900

    fix: Fix emd repository

--- backend/business/src/main/java/com/watson/business/region/domain/repository/EmdRepository.java
+++ backend/business/src/main/java/com/watson/business/region/domain/repository/EmdRepository.java
@@ -8,13 +8,13 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface EmdRepository extends JpaRepository<Emd, String> {
-//    @Query(value = "select distinct gungu_name from business.emd where sido_name = :sido and gungu_name is not null", nativeQuery = true)
-//    List<String> findGunguNamesBySido(@Param("sido") String sido);
-//
-//    @Query(value = "SELECT DISTINCT court_code, donglee_name  FROM business.emd WHERE gungu_name = :gunguName AND NOT dong_name IS NULL", nativeQuery = true)
-//    List<Object[]> findCourtCodesByGungu(@Param("gunguName") String gunguName);
-//
-//    @Query("SELECT DISTINCT e.dongleeName FROM Emd e WHERE e.courtCode = :courtCode")
-//    String findDongleeNameByCourtCode(@Param("courtCode") String courtCode);
+    @Query(value = "select distinct gungu_name from business.emd where sido_name = :sido and gungu_name is not null", nativeQuery = true)
+    List<String> findGunguNamesBySido(@Param("sido") String sido);
+
+    @Query(value = "SELECT DISTINCT court_code, donglee_name  FROM business.emd WHERE gungu_name = :gunguName AND NOT dong_name IS NULL", nativeQuery = true)
+    List<Object[]> findCourtCodesByGungu(@Param("gunguName") String gunguName);
+
+    @Query("SELECT DISTINCT e.dongleeName FROM Emd e WHERE e.courtCode = :courtCode")
+    String findDongleeNameByCourtCode(@Param("courtCode") String courtCode);
 
 }
