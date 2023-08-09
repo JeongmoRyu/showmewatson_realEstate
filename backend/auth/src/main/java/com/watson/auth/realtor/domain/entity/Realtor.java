432f93ca216b08ab56536c64cc0f41adfda3aabc
오채영 <web3765@gmail.com>
2023-08-09 16:37:20 +0900

    feat: Implement wish CRUD

---
+++ backend/auth/src/main/java/com/watson/auth/realtor/domain/entity/Realtor.java
@@ -0,0 +1,48 @@
+package com.watson.auth.realtor.domain.entity;
+
+import com.watson.auth.util.BaseEntity;
+import javax.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+@Data
+@Table(name="realtors")
+public class Realtor extends BaseEntity {
+
+    @Id
+    private String id;
+
+    // 중개사 정보
+    @Column(nullable = false)
+    private String license;
+
+    @Column(nullable = false)
+    private String realtorName;
+
+    @Column(nullable = false)
+    private String phoneNumber;
+
+    @Column(nullable = false)
+    private String profileImg;
+
+    // 중개사무소 정보
+    @Column(nullable = false)
+    private String registId;
+
+    @Column(nullable = false)
+    private String agencyName;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private String tel;
+
+    @Column(nullable = false)
+    private String agencyImg;
+
+}
