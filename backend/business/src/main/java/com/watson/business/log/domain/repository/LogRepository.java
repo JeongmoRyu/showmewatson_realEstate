a2212cda5ba0768443546ebe19ca509b90bc1319
김기헌 <cbj2741@naver.com>
2023-08-09 14:23:50 +0900

    feat: Multiple DB Connect, Log 남기기, Log 조회

--- backend/business/src/main/java/com/watson/business/log/domain/repository/LogRepository.java
+++ backend/business/src/main/java/com/watson/business/log/domain/repository/LogRepository.java
@@ -1,16 +1,17 @@
-package com.watson.log.domain.repository;
+package com.watson.business.log.domain.repository;
 
-import com.watson.log.domain.entity.ViewLog;
+import com.watson.business.log.domain.entity.ViewLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.Date;
 import java.util.List;
 
 @Repository
-public interface LogRepository extends JpaRepository<ViewLog,Long> {
+public interface LogRepository extends JpaRepository<ViewLog, Long> {
     @Query("SELECT DISTINCT l FROM ViewLog l " +
             "WHERE l.houseId=:houseId " +
             "AND l.userId = :userId " +
@@ -20,4 +21,7 @@ public interface LogRepository extends JpaRepository<ViewLog,Long> {
             @Param("userId") Long userId,
             @Param("logDate") Date logDate
     );
+
+    boolean existsByUserIdAndHouseIdAndLogDate(String userId, Long houseId, LocalDate today);
 }
+
